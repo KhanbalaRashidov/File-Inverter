0a7c2051c0a5310a844274eaeb71211c10b2cf35
KhanbalaRashidov <reshidovxanbala@gmail.com>
2022-06-22 22:54:26 +0400

    Finish

--- Inverter-ConsoleApp/Program.cs
+++ Inverter-ConsoleApp/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http.Headers;
+using System.IO;
+
+string[] lines= File.ReadAllLines("C:\\Users\\KhanbalaRashidov\\Desktop\\input.txt");
+string path = "C:\\Users\\KhanbalaRashidov\\Desktop\\output.txt";
+FileStream f = new FileStream(path, FileMode.OpenOrCreate);
+StreamWriter s = new StreamWriter(f);
+if (lines.Length >= 10)
+{
+    foreach (var item in lines)
+    {
+        HttpClient client = new HttpClient();
+        client.BaseAddress = new Uri("https://localhost:44315/api/inverter/");
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("apllication/json"));
+        HttpResponseMessage response = await client.GetAsync($"{item}");
+        var data = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+        s.WriteLine(data);
+    }
+}
+s.Close();
+f.Close();
+
+//HttpClient client = new HttpClient();
+//client.BaseAddress = new Uri("https://localhost:44315/api/inverter/");
+//client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("apllication/json"));
+//HttpResponseMessage response = await client.GetAsync("line");
+
+//Console.WriteLine( await response.Content.ReadAsStringAsync());
+Console.ReadLine();
+
---
+++ InverterService-API/Controllers/InverterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InverterService_API.Controllers
+{
+    [Route("api/inverter")]
+    [ApiController]
+    public class InverterController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{word}")]
+        public IActionResult ConvertString(string word)
+        {
+            string reverseWordString = string.Join(" ", word
+             .Split(' ')
+             .Select(x => new String(x.Reverse().ToArray())));
+
+            return Ok(reverseWordString);
+        }
+    }
+}
